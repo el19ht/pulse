ff9ca27ea37050dab0cfc9548b377c731ed1c6cb
stradiuarius <58781683+stradiuarius@users.noreply.github.com>
2022-02-03 13:20:44 +0200

    Update smart.php

--- mailer/smart.php
+++ mailer/smart.php
@@ -13,12 +13,12 @@ $mail->CharSet = 'utf-8';
 $mail->isSMTP();                                      // Set mailer to use SMTP
 $mail->Host = 'smtp.gmail.com';  // Specify main and backup SMTP servers
 $mail->SMTPAuth = true;                               // Enable SMTP authentication
-$mail->Username = 'mailforgames1984@gmail.com';                 // Наш логин
-$mail->Password = 'R16300g20P142';                           // Наш пароль от ящика
+$mail->Username = 'email';                 // Наш логин
+$mail->Password = '12345';                           // Наш пароль от ящика
 $mail->SMTPSecure = 'ssl';                            // Enable TLS encryption, `ssl` also accepted
 $mail->Port = 465;                                    // TCP port to connect to
  
-$mail->setFrom('mailforgames1984@gmail.com', 'Pulse');   // От кого письмо 
+$mail->setFrom('email', 'Pulse');   // От кого письмо 
 $mail->addAddress('stradiuarius@upcmaill.com');     // Add a recipient
 //$mail->addAddress('ellen@example.com');               // Name is optional
 //$mail->addReplyTo('info@example.com', 'Information');
